7ecdecd7c014471eb7a9c594643ec8c4e6c52766
Amalie Rosenberg <amal638e@stud.kea.dk@amalierosenberg.dk>
2022-06-09 21:06:15 +0200

    småændringer

---
+++ script.js
@@ -0,0 +1,16 @@
+let cont = document.querySelector(".container-produkter");
+let left = document.querySelector(".pre-btn");
+let right = document.querySelector(".nxt-btn");
+
+left.addEventListener("click", slideL);
+right.addEventListener("click", slideR);
+
+function slideL() {
+  console.log("Left!");
+  cont.scrollBy(-184.033, 0);
+}
+
+function slideR() {
+  console.log("Right!");
+  cont.scrollBy(184.033, 0);
+}
